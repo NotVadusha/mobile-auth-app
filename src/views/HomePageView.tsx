f90fb1b96f549a7f11befb8daa8ed0a075cdc453
Vadym <vadym.b@develops.today>
2024-06-16 13:29:20 +0300

    feat: setup auth screens

---
+++ src/views/HomePageView.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const HomePageView = () => {
+  return (
+    <View>
+      <Text>HomePageVoew</Text>
+    </View>
+  );
+};
+
+export default HomePageView;
---
+++ src/views/auth/LoginView.tsx
@@ -0,0 +1,15 @@
+const LoginView = () => {
+  //   const { login } = useAuth();
+
+  //   const handleLogin = async () => {
+  //     await login();
+  //   };
+
+  return (
+    <div>
+      <button>Login</button>
+    </div>
+  );
+};
+
+export default LoginView;
