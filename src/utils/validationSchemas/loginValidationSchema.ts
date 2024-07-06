284ae79d43fbb8a4449908c07ebadc5977c16aca
Vadym <vadym.b@develops.today>
2024-07-06 11:26:38 +0300

    feat: separate validation schemas

---
+++ src/utils/validationSchemas/loginValidationSchema.ts
@@ -0,0 +1,8 @@
+import * as yup from "yup";
+
+export const loginValidationSchema = yup
+  .object({
+    Username: yup.string().min(3).required(),
+    Password: yup.string().min(8).required(),
+  })
+  .required();
